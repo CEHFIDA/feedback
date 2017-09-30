cb3483fe64ab23b1c639b5727fa9c7595b318875
Vladislav <laponov@gmail.com>
2017-09-30 23:40:45 +0300

    Fix name class

--- src/Notifications/SupportNotification.php
+++ src/Notifications/SupportNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class VerifyEmailNotification extends Notification
+class SupportNotification extends Notification
 {
     use Queueable;
     
