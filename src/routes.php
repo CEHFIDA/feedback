62c3601bfb73b3a1de289efe5dab08d73a2ac6a9
Miroslav <lemmas.online@gmail.com>
2017-10-04 15:29:14 +0300

    update

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['prefix' => 'admin/feedback', 'middleware' => 'web'], function() {
+Route::group(['prefix' => config('adminamazing.path').'/feedback', 'middleware' => 'web'], function() {
 	Route::get('/', 'selfreliance\feedback\FeedbackController@index')->name('AdminFeedback');
 	Route::get('/{id}', 'selfreliance\feedback\FeedbackController@show')->name('AdminFeedbackAbout');
 	Route::post('/{id}', 'selfreliance\feedback\FeedbackController@send')->name('AdminFeedbackSend');
