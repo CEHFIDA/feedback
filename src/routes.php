61d3f71ebdd06b6864e09fc29f90e159cfdc361c
Vladislav <laponov@gmail.com>
2017-10-05 15:53:29 +0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,8 @@
 <?php
-
+Route::post('/contacts', 'selfreliance\feeedback\FeedbackController@send_contacts');
 Route::group(['prefix' => config('adminamazing.path').'/feedback', 'middleware' => ['web','CheckAccess'], function() {
 	Route::get('/', 'selfreliance\feedback\FeedbackController@index')->name('AdminFeedback');
 	Route::get('/{id}', 'selfreliance\feedback\FeedbackController@show')->name('AdminFeedbackAbout');
 	Route::post('/{id}', 'selfreliance\feedback\FeedbackController@send')->name('AdminFeedbackSend');
 	Route::delete('/{id}', 'selfreliance\feedback\FeedbackController@destroy')->name('AdminFeedbackDelete');
 });
-
-Route::post('/contacts', 'selfreliance\feeedback\FeedbackController@send_contacts');
