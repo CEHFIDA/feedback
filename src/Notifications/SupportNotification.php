9acb156f49599df10cbda5522938dbb36812a05b
Miroslav <lemmas.online@gmail.com>
2017-11-27 11:12:49 +0200

    remove trans (temp) SupportNotification

--- src/Notifications/SupportNotification.php
+++ src/Notifications/SupportNotification.php
@@ -44,7 +44,7 @@ class SupportNotification extends Notification
     {
         return (new MailMessage)
                 ->subject($this->info['subject'])
-                ->greeting( trans('translate-feedback.feedback.supportAnswer').config('app.name') )
+                ->greeting('Ответ поддержки '.config('app.name') )
                 ->line($this->info['message']);
     }
 
